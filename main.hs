cdcd208990358132eefa7d9ec04e837604a892c3
Edvin Havic <edvinhavic@gmail.com>
2017-05-09 17:34:07 +0200

    Init commit, WIP

---
+++ main.hs
@@ -0,0 +1,31 @@
+scoreMatch = 0
+scoreMismatch = -1
+scoreSpace = -1
+
+score :: Char -> Char -> Int
+score x '-' = scoreSpace
+score '-' y = scoreSpace
+score x y
+  | x == y    = scoreMatch
+  | otherwise = scoreMismatch
+
+similarityScore :: String -> String -> Int
+similarityScore (x:xs) (y:ys) = maximum [
+    similarityScore xs ys + score x y,
+    similarityScore xs (y:ys) + score x '-',
+    similarityScore (x:xs) ys + score '-' y
+  ]
+similarityScore xs [] = length xs * scoreSpace
+similarityScore [] ys = length ys * scoreSpace
+
+attachHeads :: a -> a -> [([a],[a])] -> [([a],[a])] 
+attachHeads h1 h2 aList = [(h1:xs,h2:ys) | (xs,ys) <- aList]
+
+maximaBy :: Ord b => (a -> b) -> [a] -> [a]
+maximaBy _ [] = []
+maximaBy fn (x:xs) = [x | x <- xs, fn x == maximum(map fn xs)]
+
+type AlignmentType = (String,String)
+
+optAlignments :: String -> String -> [AlignmentType]
+-- optAlignments [] ys = (replicate (length ys) '-', ys) TODO: Not done 
